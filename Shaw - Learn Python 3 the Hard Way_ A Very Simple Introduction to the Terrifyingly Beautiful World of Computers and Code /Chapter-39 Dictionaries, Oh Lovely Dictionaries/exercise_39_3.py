db16e24996b04df993de7a47c86dc478c91611fe
neeraj1909 <neeraj1909@gmail.com>
2018-06-25 21:56:13 +0530

    solution of exerciseproblem 39.3

---
+++ Shaw - Learn Python 3 the Hard Way_ A Very Simple Introduction to the Terrifyingly Beautiful World of Computers and Code /Chapter-39 Dictionaries, Oh Lovely Dictionaries/exercise_39_3.py
@@ -0,0 +1,10 @@
+"""
+Find out what you can’t do with dictionaries. A big one is that they do not have order, so try
+playing with that.
+"""
+print("1. We can use list as a dictionary key. Because in a list, items can be canged by accessing it with ")
+print("indexing while in dictionary, the must be immutable. ")
+
+print("2. Order of the dictionary items are not fixed. They will print in different order ,")
+print("each time you want to print them.")
+
