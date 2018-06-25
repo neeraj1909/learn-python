69335e6bae93ceff30aa78365284c3a634150bba
neeraj1909 <neeraj1909@gmail.com>
2018-06-25 22:34:48 +0530

    solution of the exercise problem 40.4

---
+++ Shaw - Learn Python 3 the Hard Way_ A Very Simple Introduction to the Terrifyingly Beautiful World of Computers and Code /Chapter-40 Modules, Classes, and Objects/exercise_40_4.py
@@ -0,0 +1,17 @@
+"""
+Search online for “object-oriented programming” and try to overflow your brain with what you
+read. Don’t worry if it makes absolutely no sense to you. Half of that stuff makes no sense to
+me, too.
+"""
+
+print("Object-Oriented Programming- \n")
+print("1. Class- Class tells python to make a new type of thing.\n")
+print("2. Object- It has two meanings: the most basic type of thing, and any instance of same thing.\n")
+print("3. Instance- What you get when you tell Python to create a class.\n")
+print("4. def- How you define a function inside a class.\n")
+print("5. self- Inside the functions in a class, self is a variable for the instance/object being accessed.\n")
+print("6. inheritance- The concept that one class can inherit traits from another class, much like you and your parents.\n")
+print("7. composition- The concept that a class can be composed of other classes as parts, much like how a car has wheels.\n")
+print("8. attribute- A property classes have that are from composition and are usually variables.\n")
+print("9. is-a : A phrase to say that something inherits from another, as in a 'salmon' is-a fish.\n")
+print("10. has-a: A phrase to say that something is composed of other things or has a trait, as in 'a salmon has-a mouth'.\n")
