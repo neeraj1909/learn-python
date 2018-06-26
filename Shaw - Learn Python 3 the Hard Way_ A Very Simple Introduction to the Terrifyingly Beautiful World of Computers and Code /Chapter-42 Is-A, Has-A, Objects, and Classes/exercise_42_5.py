d194a33b5cadc01c4c8bc9dc7c9cc2e82566c3e6
neeraj1909 <neeraj1909@gmail.com>
2018-06-26 18:39:57 +0530

    solution of the exercise problem 42.5

---
+++ Shaw - Learn Python 3 the Hard Way_ A Very Simple Introduction to the Terrifyingly Beautiful World of Computers and Code /Chapter-42 Is-A, Has-A, Objects, and Classes/exercise_42_5.py
@@ -0,0 +1,10 @@
+"""
+Make some new relationships that are lists and dictionaries so you can also have “has-many”
+relationships.
+"""
+
+four_wheelers = {
+    'Neeraj': ['Audi', 'Jeep', 'Toyota', 'Renault', 'BMW'],
+    'Sonu': ['Jeep'],
+    'Monu': ['Audi', 'Jeep']
+}
