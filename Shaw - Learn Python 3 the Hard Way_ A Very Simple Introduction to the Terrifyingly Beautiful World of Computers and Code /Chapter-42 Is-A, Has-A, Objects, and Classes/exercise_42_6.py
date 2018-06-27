078555ac4028baba918c3a6e173ed86d75446ec2
neeraj1909 <neeraj1909@gmail.com>
2018-06-27 14:59:59 +0530

    solution of the exercise problem 42.6

---
+++ Shaw - Learn Python 3 the Hard Way_ A Very Simple Introduction to the Terrifyingly Beautiful World of Computers and Code /Chapter-42 Is-A, Has-A, Objects, and Classes/exercise_42_6.py
@@ -0,0 +1,29 @@
+"""
+Do you think there’s such thing as an “is-many” relationship? Read about “multiple inheritance,”
+then avoid it if you can.
+"""
+
+print("I think is-many relationship is analogous to one-to-many relationship.")
+print("In a one-to-many relationship, one record in a table can be associated with one or more records in another table.")
+print("\nFor example, each customer can have many sales orders.")
+
+
+print("\nThere are five types of inheritance in python-")
+print("1. Single inheritance")
+print("2. Multiple inheritance")
+print("3. Multi-level inheritance")
+print("4. Hierarchical inheritance")
+print("5. Hybrid inheritance")
+
+print("\nMultiple Python inheritance are when multiple python classes inherit from a class.")
+
+print("An example of multiple inheritance is-")
+
+print("\n\nclass Color:")
+print("    pass")
+
+print("\nclass Fruit:")
+print("    pass")
+
+print("\nclass Orange(Color,Fruit):")
+print("    pass")
